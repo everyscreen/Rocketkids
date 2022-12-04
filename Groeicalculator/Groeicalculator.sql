7a79774297f0c75c3b3e7fe443e04219d41437ce
Walid Derey <I460519@fhict.nl>
2022-12-04 10:32:33 +0000

    Test commit

--- Groeicalculator/Groeicalculator.sql
+++ Groeicalculator/Groeicalculator.sql
@@ -19,7 +19,6 @@ Select gm_naam,   a_inw20,
 
 
 
------Rommelbak----
-
+-----Rommelbak--------
 ---a_inw20 * 100/(SELECT SUM(a_inw20) FROM cte_populatie) as 'Percentage of Total20',
 ---(INTO NewTable)
