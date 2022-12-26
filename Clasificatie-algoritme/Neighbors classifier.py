eccfc6b96059e9bbe73c24a26261cf1dfdffed13
Marlies Kolbeek <I483020@fhict.nl>
2022-12-26 13:49:27 +0000

    KNN algoritme met link naar DB Accuracy 61%

--- Clasificatie-algoritme/Neighbors classifier.py
+++ Clasificatie-algoritme/Neighbors classifier.py
@@ -1,54 +1,66 @@
-import pandas as pd
+import pandas as pd 
 import numpy as np
-import sklearn
+import pyodbc
+import sklearn 
 
 from sklearn.model_selection import train_test_split #(split de data voor training en test)
 from sklearn.preprocessing import StandardScaler #preprocessor voor minimal bias
 from sklearn.neighbors import KNeighborsClassifier #de KNN clasificatie algoritme
 from sklearn.metrics import confusion_matrix #test het model voor accuracy
-from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 
-dataset = pd.read_excel(r'C:\Users\mkolb\Documents\Word documents\HBO ICT\Semester 3\datasets\MLSampledataset2.xlsx')
+server = 'tcp:marlies.database.windows.net'
+database = 'rocketdb2'
+username = 'rocketadmin'
+password = 'Marlies123!'
+driver= '{ODBC Driver 17 for SQL Server}'
+conn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' +
+    server + ';PORT=1433;DATABASE=' + database +
+    ';UID=' + username + ';PWD=' + password)
+
+cursor = conn.cursor()
+
+query = "SELECT * FROM [dbo].[MLkindplaats_kids_testset]"
+dataset = pd.read_sql(query, conn)
+
 print(dataset.head())
 
-#split de data in training en test data
-#column 1, 2 en 3 zijn data. in 4 staat het antwoord
-#pakt geen string dus data onder column 1
+# Extract the features and labels
+# the features of a dataset are the characteristics or attributes of the data, 
+# while the label is the target variable that we are trying to predict.
+
 x = dataset.iloc[:, 1:3]
 y = dataset.iloc[:, 3]
 
-#split de data. 0.2 staat voor 20% van de data wat apart wordt gezet
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 0, test_size = 0.2)
+#split de data. 0.5 staat voor 50% van de data wat apart wordt gezet
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 1, test_size = 0.50)
 
-#standard scaler
-#uitzoeken wat dit nu doet
+print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
+
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
 x_test = sc_x.fit_transform(x_test)
 
-# getal van de neighbors afstand berekenen:
-import math
-a = math.sqrt(len(y_test))
-print('math neigbors classifier =',(a))
+# Create a KNN classifier 5 is normal. Euclidean metric is het meest gangbaar
+knn = KNeighborsClassifier(n_neighbors=2, metric='euclidean')
 
-#define the model KNN, metric = 'euclidian' kan niet. nu standaard gehouden checken wat dat doet
-classifier = KNeighborsClassifier(n_neighbors=3, p=1)
+# Train the classifier using the training data
+knn.fit(x_train, y_train)
 
-#fit model
-classifier.fit(x_train, y_train)
+# Predict the labels for the test data
+y_pred = knn.predict(x_test)
+print(y_pred)
 
-#predict test results
-y_pred = classifier.predict(x_train)
-print('y_pred =',(y_pred))
+print(x_train.shape)
+print(y_pred.shape)
 
-#evaluate model confusion matrix ff nader onderzoeken
-cm = confusion_matrix(y_test, y_pred)
-print('confusion matrix =', (cm))
+if y_train.shape == y_pred.shape:
+    print("The input arrays have the same number of samples.")
+else:
+    print("The input arrays have different numbers of samples.")
 
-#f1 score
-print('f1 score =',(f1_score(y_test, y_pred)))
-#accuracy score
-print('accuracy score =',(accuracy_score(y_test, y_pred)))
+# Calculate the accuracy score of the KNN model
+accuracy = accuracy_score(y_train, y_pred)
 
-#hoe kan ik de overgebleven data classificeren?
+# Print the accuracy score
+print("Accuracy: {:.2f}".format(accuracy))
